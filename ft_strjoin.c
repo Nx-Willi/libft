e010fad8d64eb5d8c88ab8d9310bdd2e3cb801b8
William DEBOTTE <wdebotte@e2r12p18.clusters.42paris.fr>
2021-12-12 17:34:33 +0100

    Little modifications on strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: wdebotte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 13:06:33 by wdebotte          #+#    #+#             */
-/*   Updated: 2021/12/01 16:41:19 by wdebotte         ###   ########.fr       */
+/*   Updated: 2021/12/12 16:22:01 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,53 +37,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	i = 0;
 	while (s1[i] != '\0')
-	{
-		buffer[i] = s1[i];
-		i++;
-	}
+		buffer[i++] = s1[i];
 	while (s2[i - size_s1] != '\0')
-	{
-		buffer[i] = s2[i - size_s1];
-		i++;
-	}
+		buffer[i++] = s2[i - size_s1];
 	buffer[i] = '\0';
 	return (buffer);
 }
-
-/* ---- Old version without malloc protect because of the number of lines
-
-static int	ft_strjoinlen(char const *s1, char const *s2)
-{
-	return (ft_strlen(s1) + ft_strlen(s2));
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	int		i;
-	int		n;
-	char	*buffer;
-
-->	if (s1 == NULL || s2 == NULL)
-->		return (ft_mallocprotect(s1, s2));
-	buffer = malloc(sizeof(char) * ft_strjoinlen(s1, s2) + 1);
-	if (buffer == NULL)
-		return (NULL);
-	i = 0;
-	n = 0;
-	while (i + n < ft_strjoinlen(s1, s2))
-	{
-		if (s1[i] != '\0')
-		{
-			buffer[i + n] = s1[i];
-			i++;
-		}
-		else
-		{
-			buffer[i + n] = s2[n];
-			n++;
-		}
-	}
-	buffer[i + n] = '\0';
-	return (buffer);
-}
-*/
