14c040591c3a495a28430c81f1e4c226ed7d123f
William <william.debotte77@gmail.com>
2022-01-08 20:06:23 +0100

    Add \n on putstr

--- srcs/str/ft_putstr.c
+++ srcs/str/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: wdebotte <wdebotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 19:42:46 by wdebotte          #+#    #+#             */
-/*   Updated: 2022/01/08 19:43:59 by wdebotte         ###   ########.fr       */
+/*   Updated: 2022/01/08 20:05:33 by wdebotte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,5 @@ void	ft_putstr(const char *str)
 	if (str == NULL)
 		return ;
 	write(1, str, ft_strlen(str));
+	ft_putchar('\n');
 }
