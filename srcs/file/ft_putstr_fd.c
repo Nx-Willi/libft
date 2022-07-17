3c1d787ee4262c6c411910cc4cd9e25d4e015661
William DEBOTTE <wdebotte@student.42.fr>
2022-07-18 00:32:39 +0200

    Fix student mail in header

--- srcs/file/ft_putstr_fd.c
+++ srcs/file/ft_putstr_fd.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wdebotte <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: wdebotte <wdebotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 14:45:39 by wdebotte          #+#    #+#             */
 /*   Updated: 2022/05/27 11:50:29 by wdebotte         ###   ########.fr       */
--- srcs/str/ft_strjoin.c
+++ srcs/str/ft_strjoin.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wdebotte <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: wdebotte <wdebotte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 13:06:33 by wdebotte          #+#    #+#             */
 /*   Updated: 2022/02/08 14:19:39 by wdebotte         ###   ########.fr       */
